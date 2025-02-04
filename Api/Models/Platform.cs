8aaab025d8928f8ab2ea6666b7c0d7167f0e56cf
Hamza Susam <hamzassm18@gmail.com>
2025-02-04 23:26:02 +0300

    feat: api => sql server connection to docker container added. id props type changed

--- Api/Models/Platform.cs
+++ Api/Models/Platform.cs
@@ -5,7 +5,7 @@ namespace GraphQLWithNet8.Models;
 public class Platform
 {
     [Key]
-    public string Id { get; set; }
+    public int Id { get; set; }
     [Required]
     public string Name { get; set; }
     public string LicenseKey { get; set; }
